3c75e25b7d44db1e6b5d45aff472d8a6c2bca83d
Anna Voyteshchuk <anna.voyteshchuk@gmail.com>
2026-04-01 23:03:20 +0500

    feat: finish task6

---
+++ task-5/client/tradeware-client/src/test/java/com/example/tradeware_client/TradewareClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradeware_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradewareClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
